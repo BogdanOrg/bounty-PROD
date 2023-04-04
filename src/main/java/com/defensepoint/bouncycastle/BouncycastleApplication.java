3b25b6ab49de64be0d46e2ca2cc7ae2607aab3f6
bogdanganzha <121808191+bogdanganzha@users.noreply.github.com>
2023-04-04 14:19:28 +0200

    Update BouncycastleApplication.java

--- src/main/java/com/defensepoint/bouncycastle/BouncycastleApplication.java
+++ src/main/java/com/defensepoint/bouncycastle/BouncycastleApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BouncycastleApplication {
+    
 
     public static void main(String[] args) {
         SpringApplication.run(BouncycastleApplication.class, args);
